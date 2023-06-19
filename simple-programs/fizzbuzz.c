c0af5e56560b40c4bf256aa3829bc6c24c46bc1e
kahenyamercy <kahenyamercy5@gmail.com>
2023-06-19 09:52:53 +0000

    feat: prints fizz buzz

---
+++ simple-programs/fizzbuzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the FizzBuzz sequence from 1 to 100
+ *              following specific conditions.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+	printf("\n");
+	return (0);
+}
